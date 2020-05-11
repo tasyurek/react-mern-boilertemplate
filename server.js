48da7e17cae3d2b0a9d4e4b75318bfcffe056366
onurcantasyurek <onurcantasyurek@gmail.com>
2020-05-12 00:21:40 +0300

    Add static file

--- server.js
+++ server.js
@@ -4,4 +4,14 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+console.log(process.env.NODE_ENV);
+
+const publicPath = path.join(__dirname, "client", "build");
+
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
 app.listen(port, () => console.log("server is runnig"));
